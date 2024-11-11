a47ab3ccde80c62a1858d6274a7ae7fa907f1f7b
murray-pocha <murray.pocha@gmail.com>
2024-11-11 14:41:57 -0700

    refactored code to be more usable in testing with chai/mocha

--- breedFetcher.js
+++ breedFetcher.js
@@ -3,11 +3,10 @@ const needle = require('needle');
 const apiKey = '';
 const apiUrl = 'https://api.thecatapi.com/v1/breeds/search';
 
-const fetchBreedData = async function(breedName) {
+const fetchBreedDescription = async function(breedName, callback) {
 
   try {
     const url = `${apiUrl}?q=${breedName}`;
-
     const response = await needle('get', url, {
       headers: {
         'x-api-key': apiKey, // if needed.
@@ -20,27 +19,17 @@ const fetchBreedData = async function(breedName) {
       if (data.length > 0) {
 
         const firstBreed = data[0];
-
-        console.log(`Description for breed: '${breedName}'.`);
-        console.log(firstBreed.description);
+        callback(null, firstBreed.description);
 
       } else {
-        console.log(`No data found on current breed '${breedName}'.`);
+        callback(null, undefined);
       }
     } else {
-      console.log(`Error: status code ${response.statusCode}`);
-
+      callback(`Error: status code ${response.statusCode}`);
     }
   } catch (error) {
-    console.error('Error fetching data:', error.message);
+    callback(`Error fetching data: ${error.message}`);
   }
 };
 
-const breedName = process.argv[2];
-
-if (!breedName) {
-  console.log('Provide a breed name in the command line, please.');
-  process.exit(1);
-}
-
-fetchBreedData(breedName);
+module.exports = { fetchBreedDescription };
